4d68ea3994058f34bdcb01f0f2c780b6d39bb620
WooFoo <sujanstha3220@gmail.com>
2024-07-06 23:41:23 -0400

    first commit

---
+++ app/api/route.ts
@@ -0,0 +1,64 @@
+import fs from "fs";
+import path from "path";
+import { NextRequest, NextResponse } from "next/server";
+import ytdl from "ytdl-core";
+import { promisify } from "util";
+
+const readFileAsync = promisify(fs.readFile);
+
+const downloadVideo = async (url: string) => {
+	const fileName = `video-youtube.mp4`;
+	const filePath = path.join("downloads", fileName);
+	const writeStream = fs.createWriteStream(filePath);
+
+	await new Promise((resolve, reject) => {
+		ytdl(url, { quality: "highest" })
+			.pipe(writeStream)
+			.on("finish", resolve)
+			.on("error", reject);
+	});
+
+	return filePath;
+};
+
+const processVideo = async (filePath: string) => {
+	const processedFilePath = `processed/${path.basename(filePath)}`;
+	fs.renameSync(filePath, processedFilePath);
+	return processedFilePath;
+};
+
+export async function POST(req: NextRequest) {
+	const { url } = await req.json();
+
+	try {
+		const downloadedFilePath = await downloadVideo(url);
+		const processedFilePath = await processVideo(downloadedFilePath);
+		const fileBuffer = await readFileAsync(processedFilePath);
+
+		return new NextResponse(fileBuffer, {
+			headers: {
+				"Content-Type": "video/mp4",
+				"Content-Disposition": `attachment; filename=${path.basename(
+					processedFilePath
+				)}`,
+			},
+		});
+	} catch (error) {
+		return NextResponse.json(
+			{ error: "Failed to download or process video" },
+			{ status: 500 }
+		);
+	} finally {
+		const filePathToDelete = path.join("./processed/", "video-youtube.mp4"); // Replace with your file path
+
+		fs.unlink(filePathToDelete, (err) => {
+			if (err) {
+				console.error(
+					`Error deleting file ${filePathToDelete}: ${err}`
+				);
+				return;
+			}
+			console.log(`File ${filePathToDelete} deleted successfully`);
+		});
+	}
+}
