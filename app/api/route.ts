a4f1ed2a521dee1d07793b2c8ba4dac65d0a526e
WooFoo <sujanstha3220@gmail.com>
2024-07-07 00:49:49 -0400

    feat : instead of saving the vid file, it is directly streamed via node server

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,56 +1,3 @@
-// import fs from "fs";
-// import path from "path";
-// import { NextRequest, NextResponse } from "next/server";
-// import ytdl from "ytdl-core";
-// import { promisify } from "util";
-
-// const readFileAsync = promisify(fs.readFile);
-
-// const downloadVideo = async (url: string) => {
-// 	const fileName = `video-youtube.mp4`;
-// 	const filePath = path.join("downloads", fileName);
-// 	const writeStream = fs.createWriteStream(filePath);
-
-// 	await new Promise((resolve, reject) => {
-// 		ytdl(url, { quality: "highest" })
-// 			.pipe(writeStream)
-// 			.on("finish", resolve)
-// 			.on("error", reject);
-// 	});
-
-// 	return filePath;
-// };
-
-// const processVideo = async (filePath: string) => {
-// 	const processedFilePath = `processed/${path.basename(filePath)}`;
-// 	fs.renameSync(filePath, processedFilePath);
-// 	return processedFilePath;
-// };
-
-// export async function POST(req: NextRequest) {
-// 	const { url } = await req.json();
-
-// 	try {
-// 		const downloadedFilePath = await downloadVideo(url);
-// 		const processedFilePath = await processVideo(downloadedFilePath);
-// 		const fileBuffer = await readFileAsync(processedFilePath);
-
-// 		return new NextResponse(fileBuffer, {
-// 			headers: {
-// 				"Content-Type": "video/mp4",
-// 				"Content-Disposition": `attachment; filename=${path.basename(
-// 					processedFilePath
-// 				)}`,
-// 			},
-// 		});
-// 	} catch (error) {
-// 		return NextResponse.json(
-// 			{ error: "Failed to download or process video" },
-// 			{ status: 500 }
-// 		);
-// 	}
-// }
-
 import { NextRequest, NextResponse } from "next/server";
 import ytdl from "ytdl-core";
 
@@ -61,14 +8,10 @@ export async function POST(req: NextRequest) {
 		// Set headers for streaming response
 		const headers = {
 			"Content-Type": "video/mp4",
-			"Content-Disposition": 'attachment; filename="video.mp4"',
+			"Content-Disposition": `attachment; filename='video.mp4'`,
 		};
 
-		// Fetch YouTube video info
-		const info = await ytdl.getInfo(url);
-
-		// Get readable stream for the video
-		const videoStream = ytdl(url, { quality: "highest" });
+		const videoStream = ytdl(url, { quality: "137" });
 
 		// Stream the video directly to the client
 		return new NextResponse(videoStream as unknown as BodyInit, {
@@ -79,5 +22,18 @@ export async function POST(req: NextRequest) {
 			{ error: "Failed to download or process video" },
 			{ status: 500 }
 		);
+	}
+}
+
+export async function GET(req: NextRequest) {
+	try {
+		const searchParams = req.nextUrl.searchParams;
+		const youtubeLink = searchParams.get("link");
+		const videoInfo = await ytdl.getInfo(youtubeLink as string);
+		const vidId = videoInfo.videoDetails.videoId;
+
+		return NextResponse.json({ vidId });
+	} catch (error) {
+		return NextResponse.json({ error });
 	}
 }
